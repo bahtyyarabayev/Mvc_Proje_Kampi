aff44c82f8f2a4f47b6bf6003fb6b7449b178da5
Bahtyyar Abayev <92421009+bahtyyarabayev@users.noreply.github.com>
2025-12-28 18:40:48 +0300

    CRUD işlemleri Eklendi...

--- BusinessLayer/Concrete/CategoryManager.cs
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -1,4 +1,6 @@
-﻿using DataAccessLayer.Concrete.Repositories;
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete.Repositories;
 using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
@@ -8,25 +10,33 @@ using System.Threading.Tasks;
 
 namespace BusinessLayer.Concrete
 {
-    public class CategoryManager
+    public class CategoryManager : ICategoryService
     {
-        GenericRepository<Category> repo = new GenericRepository<Category>();
+        ICategoryDal _categroydal;
 
-        public List<Category> GetAll()
+
+        //GenericRepository<Category> repo = new GenericRepository<Category>();
+
+        //public List<Category> GetAll()
+        //{
+        //    return repo.List();
+        //}
+
+        //public void CategoryAdd(Category p)
+        //{
+        //    repo.Insert(p);
+        //    if (p.CategoryName == "" || p.CategoryName.Length <= 3 || p.CategoryDescription == "" || p.CategoryName.Length >= 50)
+        //    {
+
+        //    }
+        //    else
+        //    {
+
+        //    }
+        //}
+        public List<Category> GetList()
         {
-            return repo.List();
-        }
-
-        public void CategoryAdd(Category p)
-        {
-            if(p.CategoryName == "" || p.CategoryName.Length<=3 ||p.CategoryDescription=="" || p.CategoryName.Length>=50)
-            {
-
-            }
-            else
-            {
-                repo.Insert(p);
-            }
+            return 
         }
     }
 }
--- Mvc_Proje_Kampi/Controllers/CategoryController.cs
+++ Mvc_Proje_Kampi/Controllers/CategoryController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,11 +9,31 @@ using System.Web.Mvc;
 namespace Mvc_Proje_Kampi.Controllers
 {
     public class CategoryController : Controller
+        
     {
         // GET: Category
+        CategoryManager cm = new CategoryManager();
         public ActionResult Index()
         {
             return View();
+        }
+        
+        public ActionResult GetCategoryList()
+        {
+            //var categoryvalues = cm.GetAll();
+            return View();
+        }
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCategory(Category p)
+        {
+            //cm.CategoryAdd(p);
+            return RedirectToAction("GetCategoryList");
         }
     }
 }
